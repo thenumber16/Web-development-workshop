5433673bbf97dbc22e0b1ddd6d14ffad0cb642bf
thenumber16 <128238316+thenumber16@users.noreply.github.com>
2023-03-18 23:04:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var input = document.querySelector("button");
+console.log(input);
+
+var button = document.querySelector("button");
+console.log(button);
+button.addEventListener("click",()=>console.log(input.value));
